b5bb3d73ea8b5b8d34293fb99a6f65040dd97656
Karl Chan <exkcmailist@inbox.lv>
2023-11-10 01:42:26 +0800

    Apply linksys qsdk patch :  003_revert_spf11_incorrect_mld_validation.patch

--- net/bridge/br_multicast.c
+++ net/bridge/br_multicast.c
@@ -1701,7 +1701,7 @@ static int br_ndisc_send_na(struct net_device *dev,
 	msg = (struct nd_msg *)skb_put(skb, sizeof(*msg));
 	*msg = (struct nd_msg) {
 		.icmph = {
-			.icmp6_type = ICMPV6_NDISC_NBR_ADVERTISEMENT,
+			.icmp6_type = NDISC_NEIGHBOUR_ADVERTISEMENT,
 			.icmp6_router = false,
 			.icmp6_solicited = solicited,
 			.icmp6_override = override,
@@ -1848,7 +1848,7 @@ static void br_do_proxy_ndisc(struct sk_buff *skb, struct net_bridge *br,
 
 	msg = (struct nd_msg *)skb_transport_header(skb);
 	if (msg->icmph.icmp6_code != 0 ||
-	    msg->icmph.icmp6_type != ICMPV6_NDISC_NBR_SOLICITATION)
+	    msg->icmph.icmp6_type != NDISC_NEIGHBOUR_SOLICITATION)
 		return;
 
 	if (ipv6_addr_loopback(daddr) ||
@@ -1949,7 +1949,7 @@ static int br_multicast_ipv6_rcv(struct net_bridge *br,
 		src = eth_hdr(skb)->h_source;
 		br_ip6_multicast_leave_group(br, port, &mld->mld_mca, vid, src);
 		break;
-	case ICMPV6_NDISC_NBR_SOLICITATION:
+	case NDISC_NEIGHBOUR_SOLICITATION:
 		br_do_proxy_ndisc(skb, br, vid, port);
 		break;
 	}
